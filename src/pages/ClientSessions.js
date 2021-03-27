d6532f56dc1a193680e1cb29d1909292c8cf3c01
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-27 14:13:44 -0500

    Added pages.

---
+++ src/pages/ClientSessions.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { Container, Row, Table, Col, Button } from "react-bootstrap";
+import Header from "../components/Header";
+
+function ClientSessions () {
+    return(
+        <div>
+            <Header />
+            <Container className = "text-center justify-content-center">
+                <Row className = "text-center justify-content-center">
+                    <h1 style={{textAlign: "center"}}>Sessions</h1>
+                </Row>
+                <Row flex className = "text-center justify-content-center">
+                    <Col xs={4}>
+                        <Table bordered>
+                            <tbody>
+                                <tr>
+                                    <td><b>Trainer:</b></td>
+                                    <td>Xavier</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        <Button variant="success" className="btn-lg btn-block mt-0" href="/BookNewSession">Book a new session</Button>
+                    </Col>
+                </Row>
+
+                <br />
+
+                <Row>
+                    <Col>
+                        <h3>Last Week</h3>
+                    </Col>
+                </Row>
+                <Row className = "text-center justify-content-center">
+                    <Col xs={8}>
+                        <Table bordered striped hover>
+                            <tbody>
+                                <tr>
+                                    <td xs={2}>Mon</td>
+                                    <td xs={4}>12:15 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Wed</td>
+                                    <td xs={4}>1:45 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Fri</td>
+                                    <td xs={4}>5:15 pm</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h3>This Week</h3>
+                    </Col>
+                </Row>
+                <Row className = "text-center justify-content-center">
+                    <Col xs={8}>
+                        <Table bordered striped hover>
+                            <tbody>
+                                <tr>
+                                    <td xs={2}>Mon</td>
+                                    <td xs={4}>12:15 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Wed</td>
+                                    <td xs={4}>1:45 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Fri</td>
+                                    <td xs={4}>5:15 pm</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h3>Next Week</h3>
+                    </Col>
+                </Row>
+                <Row className = "text-center justify-content-center">
+                    <Col xs={8}>
+                        <Table bordered striped hover>
+                            <tbody>
+                                <tr>
+                                    <td xs={2}>Mon</td>
+                                    <td xs={4}>12:15 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Wed</td>
+                                    <td xs={4}>1:45 pm</td>
+                                </tr>
+                                <tr>
+                                    <td xs={2}>Fri</td>
+                                    <td xs={4}>5:15 pm</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
+    )
+}
+
+export default ClientSessions;
