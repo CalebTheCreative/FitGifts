62a5481cd449371af90b16b22fdbd80d13aaada6
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-28 16:06:11 -0500

    Updated form groups.

--- src/pages/AddClient.js
+++ src/pages/AddClient.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Form, InputGroup, Row, Col, ListGroup } from "react-bootstrap";
+import { Button, Container, Form, Row, Col, ListGroup } from "react-bootstrap";
 import Header from "../components/Header";
 
 // ===========================================================================
@@ -42,20 +42,41 @@ function AddClient() {
                 <Row className="text-center justify-content-center">
                     <h2>Add Rewards:</h2>
                 </Row>
-                <Row className="text-center justify-content-center">
+                <Row className="text-center justify-content-center" noGutters>
                     <Form>
-                        <InputGroup>
-                            <select className="custom-select" id="inlineFormCustomSelect">
-                                <option selected>Pts</option>
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="25">25</option>
-                            </select>
-                            <input type="text" className="form-control" aria-label="Text input with 2 dropdown buttons" placeholder="Reward Item" />
-                            <Button type="submit" variant="secondary" className="ml-1"><i className="fas fa-plus"></i></Button>
-                        </InputGroup>
+                        <Form.Row className="align-items-center justify-content-center">
+                            <Col xs="auto" className="my-1 mx-0">
+                                <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+                                    Add Rewards Form
+                                </Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    className="mr-sm-2"
+                                    id="formSelectRewardVal"
+                                    custom
+                                    defaultValue="Points"
+                                >
+                                    <option>Points</option>
+                                    <option>5</option>
+                                    <option>10</option>
+                                    <option>15</option>
+                                    <option>20</option>
+                                    <option>25</option>
+                                    <option>50</option>
+                                    <option>75</option>
+                                    <option>100</option>
+                                </Form.Control>
+                            </Col>
+                            <Col xs="auto" className="my-1 mx-0">
+                                <Form.Control type="text" placeholder="Reward Item" id="formInputReward" />
+                            </Col>
+                            <Col xs="auto" className="my-1 mx-0">
+                                <Button type="submit" variant="secondary"><i className="fas fa-plus"></i></Button>
+                            </Col>
+                        </Form.Row>
                     </Form>
                 </Row>
+
                 <Row className="text-center justify-content-center">
                     <Col xs={4}>
                         <ListGroup>
@@ -68,26 +89,50 @@ function AddClient() {
                 <Row className="text-center justify-content-center">
                     <h2>Rewards Criteria:</h2>
                 </Row>
+
                 <Row className="text-center justify-content-center">
                     <Form>
-                        <div className="input-group">
-                            <select className="custom-select" id="inlineFormCustomSelect">
-                                <option selected>Pts</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                            <input type="text" className="form-control" aria-label="Text input with 2 dropdown buttons" placeholder="Reward Criteria" />
-                            <button type="submit" className="btn btn-secondary ml-1"><i className="fas fa-plus"></i></button>
-                        </div>
+                        <Form.Row className="align-items-center justify-content-center">
+                            <Col xs="auto" className="my-1">
+                                <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+                                    Add Criteria Form
+                                </Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    className="mr-sm-2"
+                                    id="formSelectCritVal"
+                                    custom
+                                    defaultValue="Points"
+                                >
+                                    <option>Points</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>10</option>
+                                    <option>15</option>
+                                    <option>20</option>
+                                    <option>25</option>
+                                    <option>50</option>
+                                    <option>75</option>
+                                    <option>100</option>
+                                </Form.Control>
+                            </Col>
+                            <Col xs="auto" className="my-1">
+                                <Form.Control type="text" placeholder="Reward Criteria" />
+                            </Col>
+                            <Col xs="auto" className="my-1">
+                                <Button type="submit" variant="secondary"><i className="fas fa-plus"></i></Button>
+                            </Col>
+                        </Form.Row>
                     </Form>
                 </Row>
+
                 <Row className="text-center justify-content-center">
                     <Col xs={4}>
                         <ListGroup>
-                            <ListGroup.Item> {/* Sample */}1 pt for ever completed session</ListGroup.Item>
+                            <ListGroup.Item> {/* Sample */} 1 pt for ever completed session</ListGroup.Item>
                         </ListGroup>
                     </Col>
                 </Row>
