22ca3a18348defa7d0cac74c6f7bed7b5116e72d
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-28 23:24:55 -0500

    Updated structure and added notes for page needs.

--- src/pages/ClientRewards.js
+++ src/pages/ClientRewards.js
@@ -3,6 +3,22 @@ import { Container, Row, Table, Col, Button } from "react-bootstrap";
 import Header from "../components/Header";
 
 function ClientRewards () {
+
+	// ===========================================================================
+    // Needs
+    // ===========================================================================
+    //  - Function to handle redeeming a reward when you click on it
+    //      - Asks to confirm the trainer is redeeming that reward
+    //      - Deducts that reward's point value from the client's reward point total
+    //      - Once reward is used, route trainer to 'ClientProfile' pg
+	// 	- Function to display the reward progress until next goal using react-progress-bar
+	// 	- Function to display all of the rewards and rewards criteria with mapping
+    //  - Function to notify trainer if client selected a reward they want to redeem
+    //  - Props for:
+    //      - Client's name                     (Used in table)
+    //      - Client's rewards points           (Used in table and progress bar)
+    //      - Client's Rewards                  (Used for progress bar & buttons)
+
     return(
         <div>
             <Header />
@@ -12,14 +28,20 @@ function ClientRewards () {
                 </Row>
 
                 <Row flex className = "text-center justify-content-center">
-                    <Table bordered>
-                        <tbody>
-                            <tr>
-                                <td>Rewards Points</td>
-                                <td>5</td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                    <Col xs={6}>
+                        <Table bordered>
+                            <tbody>
+                                <tr>
+                                    <td>Name:</td>
+                                    <td>Daniel</td>
+                                </tr>
+                                <tr>
+                                    <td>Rewards Points</td>
+                                    <td>5</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
                 </Row>
                 <br />
 
@@ -28,15 +50,15 @@ function ClientRewards () {
                 </Row>
                 
                 <Row className = "text-center justify-content-center">
-                    <Col xs={8}>
+                    <Col xs={4}>
                         <Container className="border">
                             <Col xs={6}>
-                                <Container className="bg-success mx-1 my-2 text-success">.</Container>
+                                <Container className="bg-success mx-0 my-2 text-success">.</Container>
                             </Col>
                             <Col></Col>
                         </Container>
                     </Col>
-                    <Col xs={4}>
+                    <Col xs={2}>
                         <h4>50%</h4>
                     </Col>
                 </Row>
@@ -69,39 +91,40 @@ function ClientRewards () {
                     </Col>
                 </Row>
                 <br />
-                
-                <Container className="text-center justify-content-center bg-secondary text-white">
-                    <Row className="text-center justify-content-center">
-                        <h3 className="text-center">Criteria:</h3>
-                    </Row>
+                <Row className="text-center justify-content-center">
+                    <Col xs={6} className="text-center justify-content-center bg-secondary text-white my-1">
+                        <Row className="text-center justify-content-center">
+                            <h3 className="text-center my-1">Criteria:</h3>
+                        </Row>
 
-                    <Row className="text-center justify-content-center">
-                        <Col xs={2} >
-                            <h5>1 pt</h5>
-                        </Col>
-                        <Col xs={4}>
-                            <h5 className="text-left">Every completed session</h5>
-                        </Col>
-                    </Row>
+                        <Row className="text-center justify-content-center">
+                            <Col xs={4}>
+                                <p>1 pt</p>
+                            </Col>
+                            <Col>
+                                <p className="text-left">Every completed session</p>
+                            </Col>
+                        </Row>
 
-                    <Row className="text-center justify-content-center">
-                        <Col xs={2}>
-                            <h5>2 pts</h5>
-                        </Col>
-                        <Col xs={4}>
-                            <h5 className="text-left">Each goal met</h5>
-                        </Col>
-                    </Row>
+                        <Row className="text-center justify-content-center">
+                            <Col xs={4}>
+                                <p>2 pts</p>
+                            </Col>
+                            <Col>
+                                <p className="text-left">Each goal met</p>
+                            </Col>
+                        </Row>
 
-                    <Row className="text-center justify-content-center">
-                        <Col xs={2}>
-                            <h5>3 pt</h5>
-                        </Col>
-                        <Col xs={4}>
-                            <h5 className="text-left">Every month w/ 4 completed sessions</h5>
-                        </Col>
-                    </Row >
-                </Container>
+                        <Row className="text-center justify-content-center">
+                            <Col xs={4}>
+                                <p>3 pts</p>
+                            </Col>
+                            <Col>
+                                <p className="text-left">Every month w/ 4 completed sessions</p>
+                            </Col>
+                        </Row >
+                    </Col>
+                </Row>
                 <br />
                 <Row className="text-center justify-content-center">
                     <Col xs={4}>
