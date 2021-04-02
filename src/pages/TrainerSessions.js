ae8258c9bfd1d4531228bb9a49649b52766d0cb2
Luis Vela <luismvela1@aol.com>
2021-04-01 19:06:45 -0500

    added popups to trainersession and tclientsessions

--- src/pages/TrainerSessions.js
+++ src/pages/TrainerSessions.js
@@ -33,15 +33,34 @@ function TrainerSessions() {
 					</Row>
 					<Row className="text-center justify-content-center">
 						<Col xs={6}>
-							<Button block hover variant="outline-secondary">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
 								12:15 pm<span>&nbsp;with&nbsp;</span><span id="sessionName">Alex</span>
 							</Button>
-							<Button block hover variant="outline-secondary">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
 								1:45 pm<span>&nbsp;with&nbsp;</span><span id="sessionName">Bobby</span>
 							</Button>
-							<Button block hover variant="outline-secondary" href="/client-profile">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  href="/client-profile">
 								3:15 pm<span>&nbsp;with&nbsp;</span><span id="sessionName">Daniel</span>
 							</Button>
+							<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <p>What would you like to do?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                         <a href="/client-profile" button type="button" class="btn btn-primary">Clients Profile Page</a>
+                                         <a href="tel:123-456-7890" button type="button" class="btn btn-primary">Call Client</a>
+                                        <button type="button" class="btn btn-primary">Delete Session</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 						</Col>
 					</Row>
 					<br />
@@ -53,10 +72,10 @@ function TrainerSessions() {
 					</Row>
 					<Row className="text-center justify-content-center">
 						<Col xs={6}>
-							<Button block hover variant="outline-secondary">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
 								8:15 am<span>&nbsp;with&nbsp;</span><span id="sessionName">Frank</span>
 							</Button>
-							<Button block hover variant="outline-secondary">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
 								11:45 pm<span>&nbsp;with&nbsp;</span><span id="sessionName">Luke</span>
 							</Button>
 						</Col>
@@ -70,10 +89,10 @@ function TrainerSessions() {
 					</Row>
 					<Row className="text-center justify-content-center">
 						<Col xs={6}>
-							<Button block hover variant="outline-secondary">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
 								9:15 am<span>&nbsp;with&nbsp;</span><span id="sessionName">Bobby</span>
 							</Button>
-							<Button block hover variant="outline-secondary" href="/client-profile">
+							<Button block hover variant="outline-secondary" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  href="/client-profile">
 								1:45 pm<span>&nbsp;with&nbsp;</span><span id="sessionName">Daniel</span>
 							</Button>
 						</Col>
