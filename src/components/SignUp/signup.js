3d49f1365d8b0cac804f3c4fa45d3c990296ce7c
Xavier Guzman <guzmanxavi112@gmail.com>
2021-04-01 19:15:23 -0500

    updated sign up to include a handlesubmit

--- src/components/SignUp/signup.js
+++ src/components/SignUp/signup.js
@@ -1,13 +1,123 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { Col, Container, Form, Row, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { Button } from 'bootstrap';
 
 function SignUp() {
 	const [username, setUsername] = useState();
 	const [password, setPassword] = useState();
 
-    const handleSubmit(event) {
-        event.preventDefault();
-        console.log("username is " + username);
-        console.log("password is " + password);
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('username is ' + username);
+		console.log('password is ' + password);
+	};
+
+	return (
+		<div>
+			<Container className="text-center justify-content-center">
+				<Row className="text-center justify-content-center">
+					<h1 style={{ textAlign: 'center' }}>FITGIFTS</h1>
+				</Row>
+				<Row className="text-center justify-content-center">
+					<h3>SIGN UP</h3>
+				</Row>
+				<br />
+
+				<Row className="text-center justify-content-center">
+					<Col xs={8} className="text-center justify-content-center">
+						<h6>Account type:&nbsp;</h6>
+						<span>
+							<ToggleButtonGroup type="radio" name="options" defaultValue={1} className="mb-2">
+								<ToggleButton value={1}>Client</ToggleButton>
+								<ToggleButton value={2}>Trainer</ToggleButton>
+							</ToggleButtonGroup>
+						</span>
+					</Col>
+				</Row>
+
+				<Row className="text-center justify-content-center">
+					<Col xs={6}>
+						<Form onSubmit={handleSubmit}>
+							<Form.Group
+								as={Row}
+								controlId="formHorizontalFirst"
+								className="text-center justify-content-center"
+							>
+								<Col xs={12}>
+									<Form.Control type="input" placeholder="First Name" />
+								</Col>
+							</Form.Group>
+
+							<Form.Group
+								as={Row}
+								controlId="formHorizontalLast"
+								className="text-center justify-content-center"
+							>
+								<Col xs={12}>
+									<Form.Control type="input" placeholder="Last Name" />
+								</Col>
+							</Form.Group>
+
+							<Form.Group
+								as={Row}
+								controlId="formHorizontalEmail"
+								className="text-center justify-content-center"
+							>
+								<Col xs={12}>
+									<Form.Control
+										type="username"
+										placeholder="Email"
+										name="username"
+										onChange={(e) => setUsername(e.target.value)}
+									/>
+								</Col>
+							</Form.Group>
+
+							<Form.Group
+								as={Row}
+								controlId="formHorizontalPhone"
+								className="text-center justify-content-center"
+							>
+								<Col xs={12}>
+									<Form.Control type="tel" placeholder="Phone Number" />
+								</Col>
+							</Form.Group>
+
+							<Form.Group
+								as={Row}
+								controlId="formHorizontalPassword"
+								className="text-center justify-content-center"
+							>
+								<Col xs={12}>
+									<Form.Control
+										type="password"
+										placeholder="Password"
+										name="password"
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</Col>
+							</Form.Group>
+
+							<Form.Group as={Row} className="text-center justify-content-center">
+								<Col xs={12}>
+									<Button type="submit" block href="/home-trainer">
+										Sign Up
+									</Button>
+								</Col>
+							</Form.Group>
+						</Form>
+					</Col>
+				</Row>
+
+				<Row className="text-center justify-content-center">
+					<p>Already have an account? </p>
+					<span>
+						<a href="/login">&nbsp;Log in here</a>
+					</span>
+				</Row>
+			</Container>
+		</div>
+	);
 }
+export default SignUp;
