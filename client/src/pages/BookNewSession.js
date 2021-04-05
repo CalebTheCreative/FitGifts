c3b56e97d5cb171a4041527613cd51547e0d2e46
Caleb Hopkins <CalebHopkins@outlook.com>
2021-04-05 16:41:33 -0500

    Added sessions component.

--- client/src/pages/BookNewSession.js
+++ client/src/pages/BookNewSession.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Container, Row, Table, Col, Button} from "react-bootstrap";
+import { Container, Row, Table, Col, Button } from "react-bootstrap";
+import Sessions from "../components/Sessions";
 
-function BookNewSession () {
-    
+function BookNewSession() {
+
     // ===========================================================================
     // Needs
     // ===========================================================================
@@ -15,92 +16,13 @@ function BookNewSession () {
     //      - Trainer's name                    (Used in table)
     //      - Trainer's available sessions      (Used in displayed session buttons)
 
-    return(
-        <div>
-            <Container className = "text-center justify-content-center">
-                <Row className = "text-center justify-content-center">
-                    <h1 style={{textAlign: "center"}}>Book New Session</h1>
-                </Row>
-                <Row className = "text-center justify-content-center">
-                    <Col xs={4}>
-                        <Table bordered>
-                            <tbody>
-                                <tr>
-                                    <td><b>Trainer:</b></td>
-                                    <td>Xavier</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-
-                <br />
-
-                <Row>
-                    <Col>
-                        <h3>Today</h3>
-                    </Col>
-                </Row>
-                <Row className = "text-center justify-content-center">
-                    <Col xs={8}>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                12:15 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                1:45 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                5:15 pm
-                            </Button>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col>
-                        <h3>Tomorrow</h3>
-                    </Col>
-                </Row>
-                <Row className = "text-center justify-content-center">
-                    <Col xs={8}>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                12:15 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                1:45 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                5:15 pm
-                            </Button>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col>
-                        <h3>Wednesday</h3>
-                    </Col>
-                </Row>
-                <Row className = "text-center justify-content-center">
-                    <Col xs={8}>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                12:15 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                1:45 pm
-                            </Button>
-                            <Button variant="outline-success" className="btn-lg btn-block">
-                                5:15 pm
-                            </Button>
-                    </Col>
-                </Row>
-
-                <Row className = "text-center justify-content-center mt-2">
-                    <Col xs={8}>
-                        <Button variant="success" className="btn-lg btn-block mt-0" href="/Home">Done</Button>
-                    </Col>
-                </Row>
-
-            </Container>
-        </div>
+    return (
+        <Container>
+            <Row className="text-center justify-content-center">
+                <h1 style={{ textAlign: "center" }}>Book A Session</h1>
+            </Row>
+            <Sessions />
+        </Container>
     )
 }
 
