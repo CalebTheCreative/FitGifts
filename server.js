46558d3d33a182ba1947fc1c8dcbc33ad308a74a
Xavier Guzman <guzmanxavi112@gmail.com>
2021-04-04 22:59:11 -0500

    cleaned up server js and removed inused signup.js in components

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
 const express = require('express');
-const morgan = require('morgan');
-// const session = require('express-session');
-// const dbConnection = require('./database');
+const session = require('express-session');
 const mongoose = require('mongoose');
-// const MongoStore = require('connect-mongo')(session);
 const passport = require('./passport');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Route requires
 const routes = require('./routes');
+// Database
+const db = require('./models');
 
-// MIDDLEWARE
-app.use(morgan('dev'));
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitgifts', { useNewUrlParser: true });
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/public'));
+}
 
-// Sessions
-// app.use(
-// 	session({
-// 		secret: 'count-down-city', //pick a random string to make the hash that is generated secure
-// 		store: new MongoStore({ mongooseConnection: dbConnection }),
-// 		resave: false, //required
-// 		saveUninitialized: false, //required
-// 	})
-// );
+Sessions;
+app.use(
+	session({
+		secret: 'count-down-city', //pick a random string to make the hash that is generated secure
+		resave: true,
+		saveUninitialized: true, //required
+	})
+);
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session()); // calls the deserializeUser
 
 // Routes
-app.use('/routes', routes);
+app.use(routes);
 
+// Connect to the Mongo DB
+mongoose
+	.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitgifts', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log('MongoDB Connected...'))
+	.catch((err) => console.log(err));
 // Starting Server
 app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`);
+	console.log(`🌎  ==> API Server now listening on PORT: ${PORT}`);
 });
