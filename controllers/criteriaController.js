f44f963057fed0d0ec0de05b44a41e3c88265246
Caleb Hopkins <CalebHopkins@outlook.com>
2021-04-12 14:16:38 -0500

    Updated so rewards and criteria are sorted by point value.

--- controllers/criteriaController.js
+++ controllers/criteriaController.js
@@ -5,7 +5,7 @@ module.exports = {
 	findAll: function (req, res) {
 		db.Criteria
 			.find(req.query)
-			.sort({ date: -1 })
+			.sort({ critVal: +1 })
 			.then(dbModel => res.json(dbModel))
 			.catch(err => res.status(422).json(err));
 	},
