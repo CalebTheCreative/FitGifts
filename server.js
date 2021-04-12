43300f2c3ab25ffa99de4cf6f546a269f0be0e28
Caleb Hopkins <CalebHopkins@outlook.com>
2021-04-12 14:18:03 -0500

    Updated to handle user data.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const passport = require('./config/passport');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 // Route requires
 const routes = require('./routes');
 // Database
